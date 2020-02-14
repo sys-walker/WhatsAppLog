c0cf35964bcd54d2fba32acd3453a895e5747f0d
sys-walker <56586435+sys-walker@users.noreply.github.com>
2020-02-14 17:13:13 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
 
-# https://github.com/rizwansoaib/whatsapp-monitor/find/master
+# inspiration https://github.com/rizwansoaib/whatsapp-monitor/
 from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 import psutil, os, sys
